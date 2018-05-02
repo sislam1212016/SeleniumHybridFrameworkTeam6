5c45ebee493edf8c1c9f146affc2ca7429e78fb1
ismayatikiran <37750299+ismayatikiran@users.noreply.github.com>
2018-05-02 19:36:49 -0400

    2nd test commit

--- UnitedHealthCare/src/test/java/testUnitedHealthCare/BrowseUHC.java
+++ UnitedHealthCare/src/test/java/testUnitedHealthCare/BrowseUHC.java
@@ -1,12 +1,34 @@
 package testUnitedHealthCare;
 
 import mainUnitedHealthCare.BrowseToUHC;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class BrowseUHC extends BrowseToUHC {
 
-    @Test
-    public void browseUHC(){
-        browseToUHC();
+    public class BrowseUHC extends BrowseToUHC {
+
+        String uhcURL = "https://www.uhc.com/";
+        String plans = "https://www.aarpmedicareplans.com";
+
+
+        @Test
+        public void uhcBrowserLaunch(){
+            wChromeDriver.get(uhcURL);
+        }
+
+        @Test
+        public void findmyplans(){
+            wChromeDriver.get(plans);
+
+        }
+
+
+        @Test
+        public void uhcLogin(){
+            wChromeDriver.get(uhcURL);
+            wChromeDriver.findElement(By.xpath("//div[@class='login section']//button[@id='loginmenubutton']")).click();
+        }
+
+
     }
-}
+
