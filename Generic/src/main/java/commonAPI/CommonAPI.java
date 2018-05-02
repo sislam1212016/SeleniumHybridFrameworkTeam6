1580c2afd5dcf53c0d19f53324239a5ce0f50aa6
mohammadsabuz <mohammadsabuz@gmail.com>
2018-05-01 23:31:19 -0400

    CommonAPI Updates

--- Generic/src/main/java/commonAPI/CommonAPI.java
+++ Generic/src/main/java/commonAPI/CommonAPI.java
@@ -2,16 +2,35 @@ package commonAPI;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
 
 public class CommonAPI {
-
-    public WebDriver wChromeDriver;
+ // ================================
+    /* Uncomment the driver you need  from @BeforeMethod */
+ // ================================
+    public WebDriver wChromeDriver; //windows Chrome
+    public WebDriver wGeckoDriver; //windows Gecko
+    public WebDriver mChromeDriver; //mac Chrome
+    public WebDriver mGeckoDriver; //mac Gecko
 
     @BeforeMethod
     public void before(){
+        //windows Chrome
         System.setProperty("webdriver.chrome.driver", "../Generic/browser-driver/chromedriver.exe");
         wChromeDriver = new ChromeDriver();
-    }
 
+//        //windows Gecko
+//        System.setProperty("webdriver.gecko.driver", "../Generic/browser-driver/geckodriver.exe");
+//        wGeckoDriver = new FirefoxDriver();
+//
+//        //mac Chrome
+//        System.setProperty("webdriver.gecko.driver", "../Generic/browser-driver/chromedriver");
+//        wGeckoDriver = new FirefoxDriver();
+//
+//        // mac Gecko
+//        System.setProperty("webdriver.gecko.driver", "../Generic/browser-driver/geckodriver");
+//        wGeckoDriver = new FirefoxDriver();
+
+    }
 }
